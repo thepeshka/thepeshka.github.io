159a56c3aaf35bc5d0ee8ea2030743ce15511b1f
thepeshka <ilya@thepeshka.ru>
2022-07-06 09:37:57 +0000

    https://github.com/thepeshka/me.thepeshka.ru/commit/$GITHUB_SHA

---
+++ precache-manifest.977015f3611ded3889ac78b1669c6aa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f00d763279105cba01b90e3742f6125d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a931b7af4f9ac4fb6cbb",
+    "url": "/static/css/main.be32b4f7.chunk.css"
+  },
+  {
+    "revision": "5c89969ba6854309c3f3",
+    "url": "/static/js/2.ae9b6fc9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ae9b6fc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a931b7af4f9ac4fb6cbb",
+    "url": "/static/js/main.7ba331eb.chunk.js"
+  },
+  {
+    "revision": "94fc7b8e8216a28c7f0b",
+    "url": "/static/js/runtime-main.e00cf3ba.js"
+  }
+]);
